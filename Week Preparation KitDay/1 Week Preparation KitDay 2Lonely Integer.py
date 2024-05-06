1694a6452c1a6025c226629b7b1696da43cc2020
khawla-Moh <khawlaMoh00@gmail.com>
2024-05-07 01:09:35 +0300

    python problem solving prepration

---
+++ Week Preparation KitDay/1 Week Preparation KitDay 2Lonely Integer.py
@@ -0,0 +1,42 @@
+#1 Week Preparation KitDay 2Lonely Integer#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'lonelyinteger' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+def lonelyinteger(a):
+    # Write your code here
+    """ 
+        c=0
+        for i in a:
+            c ^= i
+        return c    
+    """
+   
+    for i in a:
+        if a.count(i)==0:
+            return i
+            
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = lonelyinteger(a)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
