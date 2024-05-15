0bde4375ccedf0194290d78e0198a055a930dbb5
khawla-Moh <khawlaMoh00@gmail.com>
2024-05-16 00:52:12 +0300

    Interview Preparation Kits1 Week Preparation KitDay 4Recursive Digit Sum HackerRank

---
+++ Week Preparation KitDay/PrepareInterview Preparation Kits1 Week Preparation KitDay 4Grid Challenge.py
@@ -0,0 +1,43 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'gridChallenge' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING_ARRAY grid as parameter.
+#
+
+def gridChallenge(grid):
+    # Write your code here
+    for i in grid:
+        for j in i:
+            if sorted(j[0]) and sorted(j[1] and sorted([j[2]])):
+                return True
+            else:
+                return False
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        n = int(input().strip())
+
+        grid = []
+
+        for _ in range(n):
+            grid_item = input()
+            grid.append(grid_item)
+
+        result = gridChallenge(grid)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
